d3b868d1d14ed385b2df9b8e98d0f1ba797ad8df
Vitor <77399148+VitorDAlbuquerque@users.noreply.github.com>
2024-05-24 10:37:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars').engine
+const bodyParser = require('body-parser')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+
+const serviceAccount = require('./fern-87ce9-firebase-adminsdk-sjvnz-0e50365d3b.json');
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+  
+const db = getFirestore();
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+    res.render('primeira_pagina')
+})
+
+app.get ('/consulta',  async function(req, res){
+    var consulta = await db.collection('pessoas').get();
+    const dados = []; 
+    
+
+        consulta.forEach(doc => {
+            dados.push({id:doc.id,
+                data_contato:doc.get('data_contato'),
+                nome:doc.get('nome'),
+                observacao:doc.get('observacao'),
+                origem:doc.get('origem'),
+                telefone:doc.get('telefone')
+        
+            })
+        })
+    
+
+
+    res.render('consulta', {dados})
+ 
+    
+})
+
+app.put('/editar/:id', async function(req, res){
+    const editar = await db.collection('pessoas').set({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        res.redirect('/')
+    })
+})
+
+app.post('/cadastrar', function (req, res) {
+    var pessoas = db.collection('pessoas').add({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function(){
+        console.log('Pessoa cadastrada com sucesso!')
+        res.redirect('/')
+    })
+})
+
+app.listen(8081, function(){
+    console.log('Servidor ativo!')
+})
